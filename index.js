05c2d84e4a4c030e5933e336414ce54e18602506
jackbui2014 <jackbui2014@gmail.com>
2021-12-06 14:17:16 +0700

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fileControl = require('./inc/filesControl');
 const createComponent = (dir, componentName)=>{
 	const componentDir = dir+'components/'+componentName+'/';
 	const interfacesDir = dir+'interfaces/';
-	fileControl.createDir('/'+dir+componentName);
+	fileControl.createDir('/'+componentDir);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-typescript.hbs'), componentDir+'index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), componentDir+'index.spec.tsx', componentName);
   fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.scss.hbs'), componentDir+componentName+'.module.scss', componentName.toLowerCase());
@@ -18,7 +18,7 @@ exports.createComponent = createComponent;
 const createComponentList = (dir, componentName)=>{
 	const componentDir = dir+'components/'+componentName+'/';
 	const interfacesDir = dir+'interfaces/';
-	fileControl.createDir('/'+dir+componentName);
+	fileControl.createDir('/'+componentDir);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-list.hbs'), componentDir+'index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-list-item.hbs'), componentDir+'item.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), componentDir+'index.spec.tsx', componentName);
