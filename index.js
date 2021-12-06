c9b48a495f58cf183b45d301d3f85d2a3c523fa6
jackbui2014 <jackbui2014@gmail.com>
2021-12-06 13:27:06 +0700

    fix

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const createComponent = (dir, componentName)=>{
 	fileControl.createDir('/'+dir+componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-typescript.hbs'), dir+componentName+'/index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), dir+componentName+'/index.spec.tsx', componentName);
-  fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.scss.hbs'), dir+componentName+'/'+componentName+'.module.scss', componentName);
+  fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.scss.hbs'), dir+componentName+'/'+componentName+'.module.scss', componentName.toLowerCase());
 }
 exports.createComponent = createComponent;
