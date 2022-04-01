7103f7a80d75aa788f6203aa59bb3283aa488ad1
jackbui2014 <jackbui2014@gmail.com>
2022-04-01 15:01:10 +0700

    modify components

--- index.js
+++ index.js
@@ -5,32 +5,46 @@ var fs = require("fs"),
 const fileControl = require('./inc/filesControl');
 
 const createComponent = (dir, componentName)=>{
-	const componentDir = dir+'components/'+componentName+'/';
+	const componentArr = componentName.split('/');
+	let componentNameFinal = dir+'components/';
+	componentArr.each((item, index)=>{
+		componentNameFinal += item+'/';
+	});
+	const componentDir = componentNameFinal;
 	const interfacesDir = dir+'interfaces/';
 	fileControl.createDir('/'+componentDir);
 	//fileControl.createDir('/'+interfacesDir);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-typescript.hbs'), componentDir+'index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), componentDir+'index.spec.tsx', componentName);
   fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.scss.hbs'), componentDir+componentName+'.module.scss', componentName.toLowerCase());
+	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/interfaces/index.hbs'), componentDir+'type.ts', componentName);
 	//fileControl.createFileFromTemplate(path.join(__dirname, '/templates/interfaces/index.hbs'), interfacesDir+componentName+'.tsx', componentName);
 }
 exports.createComponent = createComponent;
 
 const createComponentList = (dir, componentName)=>{
-	const componentDir = dir+'components/'+componentName+'/';
-	const interfacesDir = dir+'interfaces/';
+	const componentArr = componentName.split('/');
+	let componentNameFinal = dir+'components/';
+	componentArr.each((item, index)=>{
+		componentNameFinal += item+'/';
+	});
+	const componentDir = componentNameFinal;
 	fileControl.createDir('/'+componentDir);
-	fileControl.createDir('/'+interfacesDir);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-list.hbs'), componentDir+'index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-list-item.hbs'), componentDir+'item.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), componentDir+'index.spec.tsx', componentName);
   fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.scss.hbs'), componentDir+componentName+'.module.scss', componentName.toLowerCase());
-	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/interfaces/index.hbs'), interfacesDir+componentName+'.tsx', componentName);
+	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/interfaces/indexlist.hbs'), componentDir+'type.ts', componentName);
 }
 exports.createComponentList = createComponentList;
 
 const createComponentLayout = (dir, componentName)=>{
-	const componentDir = dir+'components/'+componentName+'/';
+	const componentArr = componentName.split('/');
+	let componentNameFinal = dir+'components/';
+	componentArr.each((item, index)=>{
+		componentNameFinal += item+'/';
+	});
+	const componentDir = componentNameFinal;
 	fileControl.createDir('/'+componentDir);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component-layout.hbs'), componentDir+'index.tsx', componentName);
 	fileControl.createFileFromTemplate(path.join(__dirname, '/templates/components/component.test.hbs'), componentDir+'index.spec.tsx', componentName);
